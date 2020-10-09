513ff4170a7cfd0e563db7d30a011ca2f4dcebd9
rti56kt <rti56kt@gmail.com>
2020-10-09 22:31:08 +0800

    [NPP1-2] Build a rough structure 1. Add a function named "npsh" running a while loop to print prompt and call corresponding function to read input, parse cmd, and do fork. 2. Add a function named "readInput" to read user input. 3. Add a function named "parseCmd" to parse user input to seperate cmds. 4. Add a function named "buildInCmd" to deal with build-in cmds. 5. Add a function named "doFork" to deal with fork.

--- npshell.cpp
+++ npshell.cpp
@@ -1,9 +1,47 @@
 #include <signal.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+void doFork(){
+    // do Fork
+}
+
+void buildInCmd(vector<string>){
+    
+}
+
+vector<vector<string>> parseCmd(string user_input){
+    // do parsing
+}
+
+string readInput(){
+    string user_input;
+
+    getline(cin, user_input, '\n');
+    if(cin.eof()) exit(EXIT_SUCCESS);
+
+    return user_input;
+}
 
 void npsh(){
+    while(true){
+        string user_input;
+        vector<vector<string>> cmds;  // Outer vector: different cmds; Inner vector: single cmd + args
 
+        // Print prompt
+        cout << "%" << flush;
+
+        user_input = readInput();
+        if(user_input.size() >= 1) cmds = parseCmd(user_input);
+        else continue;  // Means the input is a blank line
+
+        if(cmds.at(0).at(0).compare("exit") == 0 || cmds.at(0).at(0).compare("setenv") == 0 || cmds.at(0).at(0).compare("printenv") == 0) buildInCmd(cmds.at(0));
+        else doFork();
+    }
 }
 
 int main(int argc, char **argv, char **envp){
