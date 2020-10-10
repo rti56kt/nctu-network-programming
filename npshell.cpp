39057790bbba2082f8747d37b512cc20ef7036ff
rti56kt <rti56kt@gmail.com>
2020-10-10 19:20:20 +0800

    [NPP1-3] Implemented buildInCmd; parseCmd work in progress; Updated .gitignore 1. Inplmented buildInCmd 2. [WIP] parseCmd 3. Updated .gitignore

--- npshell.cpp
+++ npshell.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <vector>
+#include <sstream>
 #include <iostream>
 
 using namespace std;
@@ -10,12 +11,85 @@ void doFork(){
     // do Fork
 }
 
-void buildInCmd(vector<string>){
-    
+void buildInCmd(vector<string> cmd){
+    if(cmd.at(0).compare("exit") == 0){
+        exit(EXIT_SUCCESS);
+    }else if(cmd.at(0).compare("setenv") == 0){
+        if(cmd.size() == 3){
+            char env_name[cmd.at(1).size() + 1];
+            char env_val[cmd.at(2).size() + 1];
+
+            strcpy(env_name, cmd.at(1).c_str());
+            strcpy(env_val, cmd.at(2).c_str());
+
+            setenv(env_name, env_val, 1);
+        }else{
+            cerr << "usage: setenv [variable name] [value to assign]" << "\n";
+        }
+    }else if(cmd.at(0).compare("printenv") == 0){
+        if(cmd.size() == 2){
+            char env_name[cmd.at(1).size() + 1];
+
+            strcpy(env_name, cmd.at(1).c_str());
+
+            if(getenv(env_name) != NULL) cout << getenv(env_name) << "\n";
+        }else{
+            cerr << "usage: printenv [variable name]" << "\n";
+        }
+    }
+
+    return;
 }
 
-vector<vector<string>> parseCmd(string user_input){
-    // do parsing
+vector<vector<string>> parseCmd(string user_input, bool &pipe_in_end){
+    string token;
+    vector<string> tokens;
+    vector<string> cmd;
+    vector<vector<string>> cmds;
+    stringstream user_input_stream(user_input);
+
+    // Convert user_input to a stream
+    // Use getline() to parse the input by setting a single space char ' ' as delimeter
+    // Store all token to a vector named "tokens"
+    while(getline(user_input_stream, token, ' ')){
+        tokens.push_back(token);
+    }
+
+    // Divide tokens into cmd + args, pipe, number pipe, and io redirection
+    for(vector<string>::iterator iter = tokens.begin(); iter != tokens.end(); iter++){
+        if(*iter == "<"){
+            // Input redirection
+            cmds.push_back(cmd);
+            cmd.clear();
+            pipe_in_end = true;
+        }else if(*iter == ">"){
+            // Output redirection
+            cmds.push_back(cmd);
+            cmd.clear();
+            pipe_in_end = true;
+        }else if(*iter == "|"){
+            // Normal pipe
+            cmds.push_back(cmd);
+            cmd.clear();
+            pipe_in_end = true;
+        }else if((*iter).at(0) == '|' && isdigit((*iter).at(1)) || (*iter).at(0) == '!' && isdigit((*iter).at(1))){
+            // Number pipe
+            cmds.push_back(cmd);
+            cmd.clear();
+            pipe_in_end = true;
+        }else{
+            // Cmd + args
+            cmd.push_back(*iter);
+            pipe_in_end = false;
+        }
+    }
+
+    if(!pipe_in_end){
+        cmds.push_back(cmd);
+        cmd.clear();
+    }
+
+    return cmds;
 }
 
 string readInput(){
@@ -29,14 +103,16 @@ string readInput(){
 
 void npsh(){
     while(true){
+        bool pipe_in_end;
         string user_input;
         vector<vector<string>> cmds;  // Outer vector: different cmds; Inner vector: single cmd + args
 
         // Print prompt
-        cout << "%" << flush;
+        cout << "% " << flush;
 
         user_input = readInput();
-        if(user_input.size() >= 1) cmds = parseCmd(user_input);
+
+        if(user_input.size() >= 1) cmds = parseCmd(user_input, pipe_in_end);
         else continue;  // Means the input is a blank line
 
         if(cmds.at(0).at(0).compare("exit") == 0 || cmds.at(0).at(0).compare("setenv") == 0 || cmds.at(0).at(0).compare("printenv") == 0) buildInCmd(cmds.at(0));
